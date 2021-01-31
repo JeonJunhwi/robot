a270b9f2556d3485e194bac0c780d66a887a5bc1
JeonJunhwi <78296146+JeonJunhwi@users.noreply.github.com>
2021-01-31 19:57:44 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    threshold += 100
+    basic.showString("" + (threshold))
+})
+function turn () {
+    magicbit.MotorRun(magicbit.Motors.M1, -255)
+    magicbit.MotorRun(magicbit.Motors.M2, 255)
+}
+input.onButtonPressed(Button.B, function () {
+    threshold += -100
+    basic.showString("" + (threshold))
+})
+function forward () {
+    magicbit.MotorRun(magicbit.Motors.M1, 255)
+    magicbit.MotorRun(magicbit.Motors.M2, 255)
+}
+let threshold = 0
+threshold = 2000
+let timer = 0
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.Strength) > threshold) {
+        turn()
+        basic.pause(1000)
+    } else {
+        forward()
+    }
+    if (input.runningTime() > timer) {
+        timer = timer + 1000
+        turn()
+        basic.pause(200)
+    }
 })
